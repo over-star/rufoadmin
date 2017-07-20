2cd909de4117cf2d2210b1dad0230a82e8685765
Unknown <775397252@qq.com>
2017-07-20 23:21:42 +0800

    cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Class Authenticate
+ * @package App\Http\Middleware
+ */
+class Cros
+{
+
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept,token');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+        return $response;
+    }
+}
